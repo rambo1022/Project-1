58931d5c048bd6546e2e298c485f4b3ceed5a0a4
Jeff Williams <jwill1258@gmail.com>
2020-03-29 14:57:35 -0400

    updates

--- Sign-up/signup.js
+++ Sign-up/signup.js
@@ -1,8 +1,49 @@
-var admin = require("firebase-admin");
+var  objPeople = [
+  {
+    username:"Jeff",
+    password:"FL2000"
+  },
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+  {
+    username:"JP",
+    password:"FL2000"
+  },
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://prime-trade.firebaseio.com"
-});
+  {
+    username:"Gus",
+    password:"FL2000"
+  },
+
+  {
+    username:"Sila",
+    password:"FL2000"
+  },
+
+  {
+    username:"Jeff",
+    password:"FL2000"
+  }
+]
+
+function getInfo() {
+  var username = document.getElementById('username').value
+  var password = document.getElementById('password').value
+
+  for(i=0; i < objPeople.length; i++) {
+    if(username == objPeople[i].username && password == objPeople[i].password) {
+      console.log(username + "is logged in!!!")
+      return
+
+    }
+
+    else {
+      console.log ("incorrect username or password")
+    
+    }
+
+  }
+
+  
+  
+
+}
